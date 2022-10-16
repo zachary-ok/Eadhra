675055012348151134425335a37e1bceb0c235e4
zachary-ok <zacharyfurbush@gmail.com>
2022-10-16 00:25:45 -0400

    test2

--- bot/index.js
+++ bot/index.js
@@ -29,7 +29,7 @@ const rest = new REST({ version: '10' }).setToken(DISCORD_TOKEN);
   try {
     console.log('Started refreshing application (/) commands.');
 
-    await rest.post(Routes.applicationCommands(DISCORD_CLIENT_ID), { body: [] });
+    await rest.post(Routes.applicationCommands(DISCORD_CLIENT_ID), { body: commands });
 
     console.log('Successfully reloaded application (/) commands.');
   } catch (error) {
