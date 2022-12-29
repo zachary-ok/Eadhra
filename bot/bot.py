e0ccea34d37a6f4d407d0fb2021fb804bb1805bd
Zachary Furbush <zacharyfurbush@gmail.com>
2022-12-29 18:32:08 -0500

    Removed line for prefix

--- bot/bot.py
+++ bot/bot.py
@@ -4,7 +4,7 @@ import os
 import asyncio
 from dotenv import load_dotenv, find_dotenv
 
-client = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+client = commands.Bot(intents=discord.Intents.all())
 load_dotenv(find_dotenv())
 
 #Startup Actions
